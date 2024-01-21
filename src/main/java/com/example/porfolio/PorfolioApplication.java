6b7090e3c94fb74391f2a401258d8177bd273f5d
Jordy Camacho <jordycamacho225@gmail.com>
2024-01-21 10:36:45 +0100

    make it better

--- src/main/java/com/example/porfolio/PorfolioApplication.java
+++ src/main/java/com/example/porfolio/PorfolioApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PorfolioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PorfolioApplication.class, args);
+		SpringApplication.run(PorfolioApplication.class, args); 
 	}
 
 }
